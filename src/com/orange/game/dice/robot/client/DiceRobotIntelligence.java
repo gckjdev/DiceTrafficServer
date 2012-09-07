a34c8f15f4a499fd0e4beb6d4e9454f5929c38e4
larmbr <nasa4836@gmail.com>
2012-09-07 12:29:29 +0800

    Small modification

--- src/com/orange/game/dice/robot/client/DiceRobotIntelligence.java
+++ src/com/orange/game/dice/robot/client/DiceRobotIntelligence.java
@@ -355,10 +355,16 @@ public class DiceRobotIntelligence {
 			if (isWild){
 				// Not so intelligent, just add 1
 				if ( intelligence < IQ_THRESHOLD ){
+					if (num + 1 - distribution[dice-1] <= UNSAFE_DIFFERENCE[playerCount-2] ) {
 						recordCall(num+1, dice,1, playerCount);
 						safe = false;
 						logger.info("<DiceRobotIntelligence> isWild & not so smart, just add one, call "
 								+ whatToCall[IDX_NUM_OF_DICE]  + " X " + whatToCall[IDX_DICE_FACE_VALUE] );
+					} else {
+						giveUpCalling = true;
+						logger.info("<DiceRobotIntelligence> isWild & not so smart, not safe to call, give up!");
+						return;
+					}
 				}
 				// Quite smart, do some deep thought.
 				else {
@@ -382,7 +388,7 @@ public class DiceRobotIntelligence {
 						}
 						// round+1 is current round, if current round is 3, it must be true,
 						// if current round is 2, it is 50% true. 
-						if ( difference < UNSAFE_DIFFERENCE[playerCount-2] && (round == 0 || round + 1 + RandomUtils.nextInt(2) > 2) ){
+						if ( difference+1 < UNSAFE_DIFFERENCE[playerCount-2] && (round == 0 || round + 1 + RandomUtils.nextInt(2) > 2) ){
 							recordCall(num+1, dice,1,playerCount);
 							safe = false;
 							logger.info("<DiceRobotIntelligence> isWild & smart, just add one, call "
