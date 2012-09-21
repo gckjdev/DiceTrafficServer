3b62491a5dc1f3457dd42e4f3b098b287d8d3c19
larmbr <nasa4836@gmail.com>
2012-09-21 13:01:41 +0800

    Trivial modification

--- src/com/orange/game/dice/robot/client/DiceRobotIntelligence.java
+++ src/com/orange/game/dice/robot/client/DiceRobotIntelligence.java
@@ -349,8 +349,8 @@ public class DiceRobotIntelligence {
 				return ;
 			}
 			
-			logger.info("Current round is Round " + (round +1)+"Robot["+nickName+"'s IQ is " + intelligence 
-					+", playerCount is " + playerCount+", benchmark is " + benchmark[playerCount-2]);
+			logger.info("Current round is Round " + (round +1)+". Robot["+nickName+"]'s IQ is " + intelligence 
+					+". PlayerCount is " + playerCount+". Benchmark is " + benchmark[playerCount-2]);
 		
 			int notWild = (isWild == false? 1 : 0);
 			// How many "dice" robot have.
