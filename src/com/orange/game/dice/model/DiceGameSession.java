cd1b02dc7470dbf05a9077db6270c9d7cdf6befe
larmbr <nasa4836@gmail.com>
2012-09-26 20:56:01 +0800

    After a game,reset the play order to clockwise and correctly select the loser player

--- src/com/orange/game/dice/model/DiceGameSession.java
+++ src/com/orange/game/dice/model/DiceGameSession.java
@@ -8,13 +8,18 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.commons.lang.math.RandomUtils;
+import org.apache.lucene.analysis.CharArrayMap.EntrySet;
+
 import com.orange.common.log.ServerLog;
 import com.orange.common.utils.RandomUtil;
 import com.orange.game.dice.statemachine.DiceGameStateMachineBuilder;
 import com.orange.game.traffic.model.dao.GameSession;
-import com.orange.game.traffic.model.dao.GameSessionConstant;
 import com.orange.game.traffic.model.dao.GameUser;
+import com.orange.game.traffic.model.manager.GameSessionManager;
+import com.orange.game.traffic.server.GameEventExecutor;
+import com.orange.network.game.protocol.constants.GameConstantsProtos;
 import com.orange.network.game.protocol.constants.GameConstantsProtos.DiceGameRuleType;
 import com.orange.network.game.protocol.constants.GameConstantsProtos.GameResultCode;
 import com.orange.network.game.protocol.message.GameMessageProtos;
@@ -61,11 +66,11 @@ public class DiceGameSession extends GameSession {
 
 	String callDiceUserId;	
 	String openDiceUserId;
-	String loserUserId = null;
 	volatile int openDiceMultiple = 1;	
 	volatile int openDiceType = DICE_OPEN_TYPE_NORMAL;
 
 	private int waitClaimTimeOutTimes;
+
 	
 	public DiceGameSession(int sessionId, String name, String password, boolean createByUser, String createBy, int ruleType,int testEnable) {
 		super(sessionId, name, password, createByUser, createBy, ruleType, testEnable);
@@ -90,7 +95,6 @@ public class DiceGameSession extends GameSession {
 		clearOpenDice();
 		isWilds = false;
 		userBetCount = 0;
-		playDirection = GameSessionConstant.CLOCK_WISE;
 	}
 	
 	private void clearCallDice() {
@@ -404,14 +408,12 @@ public class DiceGameSession extends GameSession {
 		if (allFinalCount >= currentDiceNum){
 			// call-dice user wins
 			addUserResult(callDiceUserId, winCoins, true);
-			addUserResult(openDiceUserId, lostCoins, false);
-			loserUserId = openDiceUserId;
+			addUserResult(openDiceUserId, lostCoins, false);		
 		}
 		else{
 			// open-dice user wins
 			addUserResult(openDiceUserId, winCoins, true);
 			addUserResult(callDiceUserId, lostCoins, false);
-			loserUserId = callDiceUserId;
 		}
 		
 		// for the gamblers
@@ -476,14 +478,13 @@ public class DiceGameSession extends GameSession {
 	}
 
 	public String getLoserUserId(){
-//		for (PBUserResult result : userResults.values()){
-//			if (result.getWin() == false){
-//				return result.getUserId();
-//			}
-//		}
-//		
-//		return null;
-		return loserUserId;
+		for (PBUserResult result : userResults.values()){
+			if (result.getWin() == false){
+				return result.getUserId();
+			}
+		}
+		
+		return null;
 	}
 
 	public boolean reachMaxDice(int diceNum) {
