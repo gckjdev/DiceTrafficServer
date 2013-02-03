65ad32b3dfa4dcd731e3767d18c2ba5874063a6a
Benson Peng <penglingzhe@gmail.com>
2013-02-03 15:39:57 +0800

    update pom

---
+++ commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+line=' ========= '
+echo ""
+echo $line' add files (*.java *.js) '$line
+git add *.java commit.sh *.js
+echo ''
+
+echo $line' oh, commit files. comment: '"$1"" "$line
+git commit -m "$1"
+echo ''
+
+echo $line' hey, pull code from server '$line
+git pull
+echo ''
+
+echo $line' wow, push code to server '$line
+git push
+
+echo ''
+echo $line' congratulations! hope there is no conflict! '$line
+echo ''
